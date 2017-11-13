815b9c18ce94f359c7f4b7bc99cec97da1639e91
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-13 18:51:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const drivers=["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(name){
  newDrivers=drivers.push(name);
 }
+function destructivelyPrependDriver(name){
+  newDrivers2=drivers.shift(name);
+}
